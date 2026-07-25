9ba94005936f664cf7baaf5a817e2c98a36b7b88
GAkyianu <goddie.akyianu@gmail.com>
2026-07-25 17:33:01 +0100

    Added all assignment project folders to main repository

---
+++ ConditionalStatementsChallenge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ask the user for a number
+        Console.WriteLine("Enter a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Check the number using if, else if, and else
+        if (number > 0)
+        {
+            // This block runs if the number is greater than zero
+            Console.WriteLine("The number is positive.");
+        }
+        else if (number < 0)
+        {
+            // This block runs if the number is less than zero
+            Console.WriteLine("The number is negative.");
+        }
+        else
+        {
+            // This block runs only if the number is exactly zero
+            Console.WriteLine("The number is zero.");
+        }
+
+        // Pause so the console doesn't close immediately
+        Console.WriteLine("Press Enter to exit.");
+        Console.ReadLine();
+    }
+}
+
---
+++ TernaryOperatorChallenge/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TernaryOperatorChallenge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TernaryOperatorChallenge")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d43ee3b6-e597-4505-a9a5-ed6df80aede8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
